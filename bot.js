99bc9d6ec04fd0921104c151f0f21c6729b4703f
k3in0x <43535917+k3in0x@users.noreply.github.com>
2019-11-21 08:28:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const token = process.env.token;
+ 
  
 
 client.on('ready', () => {
@@ -12,18 +14,8 @@ client.on('ready', () => {
 
  
 
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
- 
-
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(token).catch(err => console.log(err));
+
+//"token" is the Client Secret
